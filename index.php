cba3086fc2b441a97b402651d0a99d70d3a4d22d
yulia <yulia.v.tsyba@gmail.com>
2017-04-13 14:20:31 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="HandheldFriendly" content="false" />
+  <meta name="viewport" content="user-scalable=yes, width=device-width" />
+  <!--[if IE]>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <![endif]-->
+  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+  </head>
+  <link href="index/css/preview.css" rel="stylesheet" type="text/css">
+<body>
+<div class="pages-wrapper">
+  <div id="logo">
+    <img src="theme/images/logo.png" alt=""/>
+  </div>
+  <ul>
+    <li>
+      <a href="name.php"><img src="index/name-s.jpg" alt=""/></a>
+      <p>
+        name
+        <a href="index/name.jpg">Preview</a>
+      </p>
+    </li>
+  </ul>
+</div>
+</body>
+</html>
+
---
+++ name.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<?php $title = 'name'; ?>
+<?php include 'tpl/includes/head.inc'; ?>
+<body class="front">
+<div class="outer-wrapper">
+  <div class="inner-wrapper container">
+    <?php include 'tpl/layout/header.inc'; ?>
+    <div class="content-wrapper">
+      <div class="media-wrapper">
+        <!--      video-->
+        <img src="theme/images/tmp/tmp-video.jpg" alt="">
+      </div>
+      <div class="media-desс">
+        <h4>iHeartCountry</h4>
+        <div class="date">May 6, 2017</div>
+      </div>
+      <div class="text-inner">
+        <h3 class="rtecenter">The iHeartCountry</h3>
+        <p class="rtecenter">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque debitis, delectus deleniti, doloribus eos hic id, inventore iste laboriosam laudantium necessitatibus nesciunt optio quaerat quod rerum sapiente totam veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque debitis, delectus deleniti, doloribus eos hic id, inventore iste laboriosam laudantium necessitatibus nesciunt optio quaerat quod rerum sapiente totam veniam. eniti, doloribus eos hic id, inventore iste laboriosam laudantium necessitatibus nesciunt optio quaerat quod rerum sapiente totam veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque debitis, </p>
+      </div>
+      <div class="block-media"><img src="theme/images/tmp/tmp-b.jpg" alt=""></div>
+      <h3 class="rtecenter">More From</h3>
+      <div class="items">
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+        <div class="item">
+          <div class="media-wrapper"><img src="theme/images/tmp/tmp-video-2.jpg" alt=""></div>
+        </div>
+      </div>
+    </div>
+    <?php include 'tpl/layout/footer.inc'; ?>
+  </div>
+</div>
+</body>
+</html>
